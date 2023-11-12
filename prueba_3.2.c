e9e8ad42ea7def978eedbf4b68ab6756887cf41e
aramos-m <anaana609@gmail.com>
2023-11-12 12:11:42 +0100

    ProgC01

--- prueba_3.2.c
+++ prueba_3.2.c
@@ -3,19 +3,19 @@
 #define DIM 100
 
 int main() {
-	char str1[DIM], str2[DIM];
-	char aux;
+	char str1[DIM], str2[DIM], aux;
+	int len;
 
 	printf("Introduce una palabra: ");
 	scanf("%s", str1);
 
+	len = strlen(str1);
 	strcpy(str2, str1);
+	aux = str2[0];
+	str2[0] = str2[len-1];
+	str2[len-1] = aux;
 
-	aux = str1[0];
-	str1[0] = str1[strlen(str1)-1];
-	str1[strlen(str1)-1] = aux;
-
-	printf("Si a la palabra %s, que tiene %d caracteres, le intercambiamos el primer y último carácter obtenemos la palabra %s", str2, strlen(str2), str1);
+	printf("Si a la palabra %s, que tiene %d caracteres, le intercambiamos el primer y último carácter obtenemos la palabra %s", str1, len, str2);
 
 	return 0;
 }
