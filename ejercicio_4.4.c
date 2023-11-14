be74a015887da159168db6da2b792e1cd3ea4e74
Ana Ramos Moya <144784967+aramos-m@users.noreply.github.com>
2023-11-14 13:39:52 +0100

    ProgC01

--- ejercicio_4.4.c
+++ ejercicio_4.4.c
@@ -1,83 +1,81 @@
 #include <stdio.h>
 
 #define DIM 128
-#define DIM_KDA 3
-#define DIM_STATS 5
 
 typedef struct {
 char user[DIM], champ[DIM];
-double kda[DIM_KDA];
+double kda[3];
 } summoner;
 
 int main() {
-  summoner stats[DIM_STATS];
+  summoner player[5];
   int total_kills, total_deaths, total_assists;
   
   printf("DATOS JUGADOR 1\n¿Cuál es tu nombre de invocador? ");
-  scanf("%s", stats[0].user);
+  scanf("%s", player[0].user);
   printf("¿Con qué campeón has jugado tu última partida? ");
-  scanf("%s", stats[0].champ);
+  scanf("%s", player[0].champ);
   printf("¿Cuántos asesinatos has hecho? ");
-  scanf("%lf", &stats[0].kda[0]);
+  scanf("%lf", &player[0].kda[0]);
   printf("¿Cuántas veces has muerto? ");
-  scanf("%lf", &stats[0].kda[1]);
+  scanf("%lf", &player[0].kda[1]);
   printf("¿Y cuántas asistencias has hecho? ");
-  scanf("%lf", &stats[0].kda[2]);
-  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", stats[0].user, stats[0].champ, (stats[0].kda[0]+stats[0].kda[2])/stats[0].kda[1]);
+  scanf("%lf", &player[0].kda[2]);
+  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", player[0].user, player[0].champ, (player[0].kda[0]+player[0].kda[2])/player[0].kda[1]);
   
   printf("\nDATOS JUGADOR 2\n¿Cuál es tu nombre de invocador? ");
-  scanf("%s", stats[1].user);
+  scanf("%s", player[1].user);
   printf("¿Con qué campeón has jugado tu última partida? ");
-  scanf("%s", stats[1].champ);
+  scanf("%s", player[1].champ);
   printf("¿Cuántos asesinatos has hecho? ");
-  scanf("%lf", &stats[1].kda[0]);
+  scanf("%lf", &player[1].kda[0]);
   printf("¿Cuántas veces has muerto? ");
-  scanf("%lf", &stats[1].kda[1]);
+  scanf("%lf", &player[1].kda[1]);
   printf("¿Y cuántas asistencias has hecho? ");
-  scanf("%lf", &stats[1].kda[2]);
-  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", stats[1].user, stats[1].champ, (stats[1].kda[0]+stats[1].kda[2])/stats[1].kda[1]);
+  scanf("%lf", &player[1].kda[2]);
+  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", player[1].user, player[1].champ, (player[1].kda[0]+player[1].kda[2])/player[1].kda[1]);
   
   printf("\nDATOS JUGADOR 3\n¿Cuál es tu nombre de invocador? ");
-  scanf("%s", stats[2].user);
+  scanf("%s", player[2].user);
   printf("¿Con qué campeón has jugado tu última partida? ");
-  scanf("%s", stats[2].champ);
+  scanf("%s", player[2].champ);
   printf("¿Cuántos asesinatos has hecho? ");
-  scanf("%lf", &stats[2].kda[0]);
+  scanf("%lf", &player[2].kda[0]);
   printf("¿Cuántas veces has muerto? ");
-  scanf("%lf", &stats[2].kda[1]);
+  scanf("%lf", &player[2].kda[1]);
   printf("¿Y cuántas asistencias has hecho? ");
-  scanf("%lf", &stats[2].kda[2]);
-  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", stats[2].user, stats[2].champ, (stats[2].kda[0]+stats[2].kda[2])/stats[2].kda[1]);
+  scanf("%lf", &player[2].kda[2]);
+  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", player[2].user, player[2].champ, (player[2].kda[0]+player[2].kda[2])/player[2].kda[1]);
   
   printf("\nDATOS JUGADOR 4\n¿Cuál es tu nombre de invocador? ");
-  scanf("%s", stats[3].user);
+  scanf("%s", player[3].user);
   printf("¿Con qué campeón has jugado tu última partida? ");
-  scanf("%s", stats[3].champ);
+  scanf("%s", player[3].champ);
   printf("¿Cuántos asesinatos has hecho? ");
-  scanf("%lf", &stats[3].kda[0]);
+  scanf("%lf", &player[3].kda[0]);
   printf("¿Cuántas veces has muerto? ");
-  scanf("%lf", &stats[3].kda[1]);
+  scanf("%lf", &player[3].kda[1]);
   printf("¿Y cuántas asistencias has hecho? ");
-  scanf("%lf", &stats[3].kda[2]);
-  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", stats[3].user, stats[3].champ, (stats[3].kda[0]+stats[3].kda[2])/stats[3].kda[1]);
+  scanf("%lf", &player[3].kda[2]);
+  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", player[3].user, player[3].champ, (player[3].kda[0]+player[3].kda[2])/player[3].kda[1]);
   
   printf("\nDATOS JUGADOR 5\n¿Cuál es tu nombre de invocador? ");
-  scanf("%s", stats[4].user);
+  scanf("%s", player[4].user);
   printf("¿Con qué campeón has jugado tu última partida? ");
-  scanf("%s", stats[4].champ);
+  scanf("%s", player[4].champ);
   printf("¿Cuántos asesinatos has hecho? ");
-  scanf("%lf", &stats[4].kda[0]);
+  scanf("%lf", &player[4].kda[0]);
   printf("¿Cuántas veces has muerto? ");
-  scanf("%lf", &stats[4].kda[1]);
+  scanf("%lf", &player[4].kda[1]);
   printf("¿Y cuántas asistencias has hecho? ");
-  scanf("%lf", &stats[4].kda[2]);
-  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", stats[4].user, stats[4].champ, (stats[4].kda[0]+stats[4].kda[2])/(double)stats[4].kda[1]);
+  scanf("%lf", &player[4].kda[2]);
+  printf("%s, tu KDA ratio con %s ha sido %.2lf.\n", player[4].user, player[4].champ, (player[4].kda[0]+player[4].kda[2])/player[4].kda[1]);
   
-  total_kills = stats[0].kda[0]+stats[1].kda[0]+stats[2].kda[0]+stats[3].kda[0]+stats[4].kda[0];
-  total_deaths = stats[0].kda[1]+stats[1].kda[1]+stats[2].kda[1]+stats[3].kda[1]+stats[4].kda[1];
-  total_assists = stats[0].kda[2]+stats[1].kda[2]+stats[2].kda[2]+stats[3].kda[2]+stats[4].kda[2];
+  total_kills = player[0].kda[0]+player[1].kda[0]+player[2].kda[0]+player[3].kda[0]+player[4].kda[0];
+  total_deaths = player[0].kda[1]+player[1].kda[1]+player[2].kda[1]+player[3].kda[1]+player[4].kda[1];
+  total_assists = player[0].kda[2]+player[1].kda[2]+player[2].kda[2]+player[3].kda[2]+player[4].kda[2];
   
-  printf("\nUsuarios %s, %s, %s, %s y %s, el KDA de vuestra partida ha sido %d/%d/%d y su ratio %.2lf.", stats[0].user, stats[1].user, stats[2].user, stats[3].user, stats[4].user, total_kills, total_deaths, total_assists, (total_kills+total_assists)/(double)total_deaths);
+  printf("\nUsuarios %s, %s, %s, %s y %s, el KDA de vuestra partida ha sido %d/%d/%d y su ratio %.2lf.", player[0].user, player[1].user, player[2].user, player[3].user, player[4].user, total_kills, total_deaths, total_assists, (total_kills+total_assists)/(double)total_deaths);
   
   return 0;
 }
