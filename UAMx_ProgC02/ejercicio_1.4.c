8ec9ceec4d1f026a36279ba555c682ff30750706
aramos-m <anaana609@gmail.com>
2023-12-25 22:25:07 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.4.c
+++ UAMx_ProgC02/ejercicio_1.4.c
@@ -9,10 +9,14 @@ int main(){
 	printf("Introduce el nombre de un mes en minúsculas: ");
 	fgets(month, 11, stdin);
 
+	size_t len = strlen(month);
+	if (len > 0 && month[len - 1] == '\n')
+		month[len - 1] = '\0';
+
 	if (strcmp(month, "enero")==0 || strcmp(month, "marzo")==0 || strcmp(month, "mayo")==0 || strcmp (month, "julio")==0 || strcmp (month, "agosto")==0 || strcmp(month, "octubre")==0 || strcmp(month, "diciembre")==0)
-		printf("%s tiene 30 días\n", month);
-	else if (strcmp(month, "abril")==0 || strcmp(month, "junio")==0 || strcmp(month, "septiembre")==0 || strcmp(month, "noviembre")==0)
 		printf("%s tiene 31 días\n", month);
+	else if (strcmp(month, "abril")==0 || strcmp(month, "junio")==0 || strcmp(month, "septiembre")==0 || strcmp(month, "noviembre")==0)
+		printf("%s tiene 30 días\n", month);
 	else if (strcmp(month, "febrero")==0)
 		printf("%s tiene 28/29 días\n", month);
 	else
