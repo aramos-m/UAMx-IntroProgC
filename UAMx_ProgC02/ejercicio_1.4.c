8cb1001a157ed3924ff20c85e9a64b21439472cc
aramos-m <anaana609@gmail.com>
2023-12-25 21:34:38 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.4.c
+++ UAMx_ProgC02/ejercicio_1.4.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-	char month[20];
+#define DIM 20
 
-	printf("Introduce el nombre de un mes: ");
+int main(){
+	char month[DIM];
+
+	printf("Introduce el nombre de un mes en minúsculas: ");
 	fgets(month, 11, stdin);
 
-	if (strcmp(month, "enero") == 0 || strcmp(month, "marzo") == 0 || strcmp(month, "mayo") == 0 || strcmp (month, "julio") == 0 || strcmp (month, "agosto") == 0 || strcmp(month, "octubre") == 0 || strcmp(month, "diciembre") == 0)
+	if (strcmp(month, "enero")==0 || strcmp(month, "marzo")==0 || strcmp(month, "mayo")==0 || strcmp (month, "julio")==0 || strcmp (month, "agosto")==0 || strcmp(month, "octubre")==0 || strcmp(month, "diciembre")==0)
 		printf("%s tiene 30 días\n", month);
-	else if (strcmp(month, "abril") == 0 || strcmp(month, "junio") == 0 || strcmp(month, "septiembre") == 0 || strcmp(month, "noviembre") == 0)
+	else if (strcmp(month, "abril")==0 || strcmp(month, "junio")==0 || strcmp(month, "septiembre")==0 || strcmp(month, "noviembre")==0)
 		printf("%s tiene 31 días\n", month);
-	else if (strcmp(month, "febrero") == 0)
+	else if (strcmp(month, "febrero")==0)
 		printf("%s tiene 28/29 días\n", month);
 	else
 		printf("Incorrect month name\n");
