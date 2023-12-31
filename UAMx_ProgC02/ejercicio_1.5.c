0eef818d29062e0ac4d042029711356ba36f554d
aramos-m <anaana609@gmail.com>
2023-12-31 15:46:12 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.5.c
+++ UAMx_ProgC02/ejercicio_1.5.c
@@ -11,16 +11,26 @@ int main() {
 
     printf("Introduce un día, mes y año separados por espacios: ");
     scanf("%d %d %d", &fecha1.dia, &fecha1.mes, &fecha1.anyo);
-    if (!(fecha1.dia >= 1 && fecha1.dia <= 31 && fecha1.mes >= 1 && fecha1.mes <= 12)) {
-            printf("Invalid input for the first date\n");
-            return 1;
+    if (fecha1.mes >= 1 && fecha1.mes <= 12) {
+	    if (fecha1.mes == 2 && !(fecha1.dia >= 1 && fecha1.dia <= 29))
+		    printf("Entrada no válida para la primera fecha\n");
+	    else if (fecha1.mes == 1, 3, 5, 7, 8, 10, 12 && !(fecha1.dia >= 1 && fecha1.dia <= 31))
+		    printf("Entrada no válida para la primera fecha\n");
+	    else if (fecha1.mes == 4, 6, 9, 11 && !(fecha1.dia >= 1 && fecha1.dia <= 30))
+		    printf("Entrada no válida para la primera fecha\n");
+    return 1;
     }
 
     printf("Introduce otro día, mes y año separados por espacios: ");
     scanf("%d %d %d", &fecha2.dia, &fecha2.mes, &fecha2.anyo);
-    if (!(fecha2.dia >= 1 && fecha2.dia <= 31 && fecha2.mes >= 1 && fecha2.mes <= 12)) {
-                printf("Invalid input for the second date\n");
-                return 1;
+    if (fecha2.mes >= 1 && fecha2.mes <= 12) {
+	    if (fecha2.mes == 2 && !(fecha2.dia >= 1 && fecha2.dia <= 29))
+		    printf("Entrada no válida para la primera fecha\n");
+	    else if (fecha2.mes == 1, 3, 5, 7, 8, 10, 12 && !(fecha2.dia >= 1 && fecha2.dia <= 31))
+		    printf("Entrada no válida para la primera fecha\n");
+	    else if (fecha2.mes == 4, 6, 9, 11 && !(fecha2.dia >= 1 && fecha2.dia <= 30))
+		    printf("Entrada no válida para la primera fecha\n");
+    return 1;
     }
 
     if (fecha1.anyo < fecha2.anyo)
@@ -40,5 +50,6 @@ int main() {
             else
                 printf("Las dos fechas son iguales");
         }
+    }
     return 0;
 }
