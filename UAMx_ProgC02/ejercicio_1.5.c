d68669ea28fdcfd6d7bcc8d949d6c1f7c516473a
Ana Ramos Moya <144784967+aramos-m@users.noreply.github.com>
2023-12-31 16:24:16 +0100

    ProgC02

---
+++ UAMx_ProgC02/ejercicio_1.5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+typedef struct {
+    int dia;
+    int mes;
+    int anyo;
+} Fecha;
+
+int main() {
+    Fecha fecha1, fecha2;
+
+    printf("Introduce un día, mes y año separados por espacios: ");
+    scanf("%d %d %d", &fecha1.dia, &fecha1.mes, &fecha1.anyo);
+    if (!(fecha1.dia >= 1 && fecha1.dia <= 31 && fecha1.mes >= 1 && fecha1.mes <= 12)) {
+            printf("Invalid input for the first date\n");
+            return 1;
+    }
+
+    printf("Introduce otro día, mes y año separados por espacios: ");
+    scanf("%d %d %d", &fecha2.dia, &fecha2.mes, &fecha2.anyo);
+    if (!(fecha2.dia >= 1 && fecha2.dia <= 31 && fecha2.mes >= 1 && fecha2.mes <= 12)) {
+                printf("Invalid input for the second date\n");
+                return 1;
+    }
+
+    if (fecha1.anyo < fecha2.anyo)
+        printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha1.dia, fecha1.mes, fecha1.anyo, fecha2.dia, fecha2.mes, fecha2.anyo);
+    else if (fecha1.anyo > fecha2.anyo)
+        printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha2.dia, fecha2.mes, fecha2.anyo, fecha1.dia, fecha1.mes, fecha1.anyo);
+    else {
+        if (fecha1.mes < fecha2.mes)
+            printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha1.dia, fecha1.mes, fecha1.anyo, fecha2.dia, fecha2.mes, fecha2.anyo);
+        else if (fecha1.mes > fecha2.mes)
+            printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha2.dia, fecha2.mes, fecha2.anyo, fecha1.dia, fecha1.mes, fecha1.anyo);
+        else {
+            if (fecha1.dia < fecha2.dia)
+                printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha1.dia, fecha1.mes, fecha1.anyo, fecha2.dia, fecha2.mes, fecha2.anyo);
+            else if (fecha1.dia > fecha2.dia)
+                printf("La fecha %d/%d/%d es anterior a %d/%d/%d", fecha2.dia, fecha2.mes, fecha2.anyo, fecha1.dia, fecha1.mes, fecha1.anyo);
+            else
+                printf("Las dos fechas son iguales");
+        }
+    }
+
+    return 0;
+}
