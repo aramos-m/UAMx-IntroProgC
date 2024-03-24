0c6551842052b7c15939b9451569341755763f1d
aramos-m <anaana609@gmail.com>
2024-03-24 15:13:28 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.7.c
+++ UAMx_ProgC02/ejercicio_1.7.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
-//EXCHANGE TO DATE 21 MARCH 2024
+//EXCHANGE TO DATE 24 MARCH 2024
 #define USD_PER_EUR 1.09
-#define JPY_PER_EUR 164.75
+#define JPY_PER_EUR 164.49
 #define GBP_PER_EUR 0.86
 
 int main() {
 	float euro;
-	int option;
+	char option;
 
 	printf("Introduce una cantidad en euros: ");
 	scanf("%f", &euro);
 
-	printf("Selecciona una opción:\n 1. Dólar\n 2. Yen japonés\n 3. Libra esterlina\n");
-	scanf("%d", &option);
-	
-	if (option < 1 || option > 3) {
-		printf("Incorrect option. Please, select 1, 2 or 3.\n");
-		return 1;
-	}
+	printf("Selecciona una opción:\n (D)ólar\n (Y)en japonés\n (L)ibra esterlina\n");
+	scanf(" %c", &option);
 
 	switch (option) {
-		case 1:
+		case 'D':
+		case 'd':
 			printf("%.2f euros son %.2f dólares.\n", euro, euro * USD_PER_EUR);
 		break;
 
-		case 2:				
+		case 'Y':
+		case 'y':			
 			printf("%.2f euros son %.2f yenes.\n", euro, euro * JPY_PER_EUR);
 		break;
 
-		case 3:
+		case 'L':
+		case 'l':
 			printf("%.2f euros son %.2f libras.\n", euro, euro * GBP_PER_EUR);
 		break;
+
+		default:
+			printf("Incorrect option. Please, select D/d, Y/y or L/l.\n");
 	}
 	
 	return 0;
