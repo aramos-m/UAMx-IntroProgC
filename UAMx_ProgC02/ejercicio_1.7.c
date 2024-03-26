44e679efdab402338cad4309a4c884c4e7323733
aramos-m <anaana609@gmail.com>
2024-03-26 12:36:18 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.7.c
+++ UAMx_ProgC02/ejercicio_1.7.c
@@ -4,18 +4,25 @@
 #define USD_PER_EUR 1.09
 #define JPY_PER_EUR 164.49
 #define GBP_PER_EUR 0.86
+#define DIM 100
 
 int main() {
 	float euro;
-	char option;
+	char option[DIM];
 
 	printf("Introduce una cantidad en euros: ");
 	scanf("%f", &euro);
 
 	printf("Selecciona una opción:\n (D)ólar\n (Y)en japonés\n (L)ibra esterlina\n");
-	scanf(" %c", &option);
+	scanf(" %s", &option);
+	printf("%s\n", option);
 
-	switch (option) {
+	if (!(option[1] == '\0')) {
+		printf("Incorrect option. Please, select D/d, Y/y or L/l.\n");
+		return 1;
+	}
+
+	switch (option[0]) {
 		case 'D':
 		case 'd':
 			printf("%.2f euros son %.2f dólares.\n", euro, euro * USD_PER_EUR);
