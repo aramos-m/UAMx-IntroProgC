a7db4e87a2a05409e329991fdd0c733ba9038943
aramos-m <anaana609@gmail.com>
2024-03-26 13:07:34 +0100

    ProgC02

--- UAMx_ProgC02/ejercicio_1.7.c
+++ UAMx_ProgC02/ejercicio_1.7.c
@@ -16,25 +16,24 @@ int main() {
 	printf("Selecciona una opción:\n (D)ólar\n (Y)en japonés\n (L)ibra esterlina\n");
 	scanf("\n%s", &option);
 
-	if (!(option[1] == '\0'))
+	if ((option[0] == 'D' || option[0] == 'd' || option[0] == 'L' || option[0] == 'l' || option[0] == 'Y' || option[0] == 'y') && option[1] == '\0') {
+		switch (option[0]) {
+			case 'D':
+			case 'd':
+				printf("%.2f euros son %.2f dólares.\n", euro, euro * USD_PER_EUR);
+				break;
+			case 'Y':
+			case 'y':			
+				printf("%.2f euros son %.2f yenes.\n", euro, euro * JPY_PER_EUR);
+				break;
+			case 'L':
+			case 'l':
+				printf("%.2f euros son %.2f libras.\n", euro, euro * GBP_PER_EUR);
+				break;
+		}
+		return 0;
+	}
+	else
 		printf("Incorrect option. Please, select D/d, Y/y or L/l.\n");
 	return 1;
-
-	switch (option[0]) {
-		case 'D':
-		case 'd':
-			printf("%.2f euros son %.2f dólares.\n", euro, euro * USD_PER_EUR);
-			break;
-		case 'Y':
-		case 'y':			
-			printf("%.2f euros son %.2f yenes.\n", euro, euro * JPY_PER_EUR);
-			break;
-		case 'L':
-		case 'l':
-			printf("%.2f euros son %.2f libras.\n", euro, euro * GBP_PER_EUR);
-			break;
-		default:
-			printf("Incorrect option. Please, select D/d, Y/y or L/l.\n");
-	}
-	return 0;
 }
