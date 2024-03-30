d926e98f1a7cea3324f15d2ff82959a8097c9b0b
aramos-m <anaana609@gmail.com>
2024-03-30 23:24:44 +0100

    Add test 1.2

--- UAMx_ProgC02/prueba_1.2.c
+++ UAMx_ProgC02/prueba_1.2.c
@@ -1,61 +1,38 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 typedef struct {
-	char num_str1[100];
-	char num_str2[100];
-	char num_str3[100];
+	long long int num1;
+	long long int num2;
+	long long int num3;
 } Numbers;
-
-int is_valid_num(const char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-        i++;
-    }
-    return 1;
-}
 
 int main() {
 	Numbers sort;
 
 	printf("Introduce el primer número: ");
-	scanf("%99s", sort.num_str1);
-
-	if (!is_valid_num(sort.num_str1)) {
+	if (scanf("%lld", &sort.num1) != 1) {
 		printf("Invalid input.\n");
 		return 1;
 	}
 
 	printf("Introduce el segundo número: ");
-	scanf("%99s", sort.num_str2);
-	
-	if (!is_valid_num(sort.num_str2)) {
+	if (scanf("%lld", &sort.num2) != 1) {
 		printf("Invalid input.\n");
 		return 1;
 	}
-
+	
 	printf("Introduce el tercer número: ");
-	scanf("%99s", sort.num_str3);
-
-	if (!is_valid_num(sort.num_str3)) {
+	if (scanf("%lld", &sort.num3) != 1) {
 		printf("Invalid input.\n");
 		return 1;
 	}
-
-	int num1 = atoi(sort.num_str1);
-	int num2 = atoi(sort.num_str2);
-	int num3 = atoi(sort.num_str3);
-	
-	if (num1 < num2) {
-		if (num1 < num3)
-			printf("El número %d es el menor\n", num1);
-	} else if (num2 < num3)
-		printf("El número %d es el menor\n", num2);
+		
+	if (sort.num1 <= sort.num2 && sort.num1 <= sort.num3)
+		printf("El número %lld es el menor\n", sort.num1);
+	else if (sort.num2 <= sort.num1 && sort.num2 <= sort.num3)
+		printf("El número %lld es el menor\n", sort.num2);
 	else
-		printf("El número %d es el menor\n", num3);
-return 0;	
+		printf("El número %lld es el menor", sort.num3);
+
+	return 0;
 }
